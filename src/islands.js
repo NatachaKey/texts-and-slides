a9ef94d0df1a1f0f2c8045ca977261a92c95fa26
Natacha Key <103755090+NatachaKey@users.noreply.github.com>
2022-10-25 20:43:12 +0200

    Update islands.js

--- src/islands.js
+++ src/islands.js
@@ -19,6 +19,4 @@ export const islands = [
         id:5,
         image:"https://images.unsplash.com/photo-1551418988-c21e451467b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80 "
     }
-  
-
 ]
